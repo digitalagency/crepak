e6ddf4435d987f4f69a0a8288603cd076b489314
binaystha <binaya619@gmail.com>
2017-06-14 17:21:57 +0545

    working on gallery

--- application/modules/digitalauth/views/product/listproduct.php
+++ application/modules/digitalauth/views/product/listproduct.php
@@ -18,10 +18,13 @@
 
                 <div class='box-header'>
                     <h3 class='box-title'>List of Products</h3>
+
                     <div class="pull-right">
-                        <a class="btn btn-primary" href="<?php echo base_url('digitalauth/product/addproduct')?>">Add Product </a>
+                        <a class="btn btn-primary" href="<?php echo base_url('digitalauth/product/addproduct') ?>">Add
+                            Product </a>
                     </div>
-                </div><!-- /.box-header -->
+                </div>
+                <!-- /.box-header -->
                 <div class="box-body">
                     <?php if ($this->session->flashdata('success_message') != "") { ?>
                         <div class="alert alert-success alert-dismissible" role="alert">
@@ -29,7 +32,7 @@
                                     aria-hidden="true">&times;</span></button>
                             <?php echo $this->session->flashdata('success_message'); ?>
                         </div>
-                    <?php } ;?>
+                    <?php }; ?>
                     <table id="tablelist" class="table table-bordered table-striped">
                         <thead>
                         <tr>
@@ -39,57 +42,78 @@
                             <th>Chinese Image</th>
                             <th>Created Date</th>
                             <th>Category</th>
+                            <th>Add Image</th>
                             <th>Status</th>
                             <th>Action</th>
                         </tr>
                         </thead>
                         <tbody>
                         <?php
-                        foreach($articles as $article){
-//                         ?>
+                        foreach ($articles as $article) {
+//
+                            ?>
                             <tr>
-                                <td><?php echo $article->title.' ('.$article->slug.')'?></td>
-                                <td><?php echo $article->title_cn;?></td>
+                                <td><?php echo $article->title . ' (' . $article->slug . ')' ?></td>
+                                <td><?php echo $article->title_cn; ?></td>
                                 <td>
                                     <?php
-                                    if($article->featured_img!=''){?>
-                                        <img src="<?php echo base_url().'uploads/products/thumbnail/'.$article->featured_img?>" width="150" alt="">
-                                    <?php    }
+                                    if ($article->featured_img != '') {
+                                        ?>
+                                        <img
+                                            src="<?php echo base_url() . 'uploads/products/thumbnail/' . $article->featured_img ?>"
+                                            width="150" alt="">
+                                    <?php }
                                     ?>
 
-                                    <?php //echo $article['featured_img']?>
+                                    <?php //echo $article['featured_img']
+                                    ?>
                                 </td>
                                 <td>
                                     <?php
-                                    if($article->featured_img_cn!=''){?>
-                                        <img src="<?php echo base_url().'uploads/products/thumbnail/'.$article->featured_img_cn?>" width="150" alt="">
-                                    <?php    }
+                                    if ($article->featured_img_cn != '') {
+                                        ?>
+                                        <img
+                                            src="<?php echo base_url() . 'uploads/products/thumbnail/' . $article->featured_img_cn ?>"
+                                            width="150" alt="">
+                                    <?php }
                                     ?>
 
-                                    <?php //echo $article['featured_img_cn']?></td>
-                                <td><?php echo $article->post_date?></td>
+                                    <?php //echo $article['featured_img_cn']
+                                    ?></td>
+                                <td><?php echo $article->post_date ?></td>
+
                                 <td><?php
-                                    if($article->post_parent!=0)
-                                    $categories = $this->myproduct->getProductCategory($article->post_parent);
+                                    if ($article->post_parent != 0)
+                                        $categories = $this->myproduct->getProductCategory($article->post_parent);
                                     else
                                         echo 'N/A';
 
                                     ?></td>
                                 <td>
-                                    <a href="javascript:void(0)" onclick="toggleCategoryStatus(<?php echo $article->id; ?>,<?php echo $article->status; ?>)" >
+                                    <a href="<?php echo base_url('digitalauth/gallery/listimages/' . $article->id) ?>">
+                                        <img src="<?php echo base_url(); ?>scriptscss/images/gallery.png">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="javascript:void(0)"
+                                       onclick="toggleCategoryStatus(<?php echo $article->id; ?>,<?php echo $article->status; ?>)">
                                         <?php
-                                        if($article->status=='1') {
-                                            echo '<img src="'.base_url().'scriptscss/images/check.png">';
-                                        }else{
-                                            echo '<img src="'.base_url().'scriptscss/images/uncheck.png">';
+                                        if ($article->status == '1') {
+                                            echo '<img src="' . base_url() . 'scriptscss/images/check.png">';
+                                        } else {
+                                            echo '<img src="' . base_url() . 'scriptscss/images/uncheck.png">';
                                         }
                                         ?>
                                     </a>
 
                                 </td>
                                 <td>
-                                    <a href="<?php echo base_url('digitalauth/product/editproduct/'.$article->id); ?>" title="Edit <?php echo $article->title;?>"><span class="glyphicon glyphicon-pencil"></span></a>
-                                    <a href="<?php echo base_url('digitalauth/product/deleteproduct/'.$article->id); ?>" title="Delete <?php echo $article->title;?>"><span class="glyphicon glyphicon-remove"></span></a>
+                                    <a href="<?php echo base_url('digitalauth/product/editproduct/' . $article->id); ?>"
+                                       title="Edit <?php echo $article->title; ?>"><span
+                                            class="glyphicon glyphicon-pencil"></span></a>
+                                    <a href="<?php echo base_url('digitalauth/product/deleteproduct/' . $article->id); ?>"
+                                       title="Delete <?php echo $article->title; ?>"><span
+                                            class="glyphicon glyphicon-remove"></span></a>
 
                                 </td>
                             </tr>
@@ -104,23 +128,28 @@
                             <th>Chinese Image</th>
                             <th>Created Date</th>
                             <th>Category</th>
+                            <th>Add Image</th>
                             <th>Status</th>
                             <th>Action</th>
                         </tr>
                         </tfoot>
                     </table>
-                </div><!-- /.box-body -->
-            </div><!-- /.box -->
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
 
-        </div><!-- /.col-->
-    </div><!-- ./row -->
+        </div>
+        <!-- /.col-->
+    </div>
+    <!-- ./row -->
 </section><!-- /.content -->
 
 <!-- Main content -->
 <script src="<?php echo base_url(); ?>scriptscss/admin/bootstraps/js/bootbox.min.js" type="text/javascript"></script>
 <script>
-    function toggleCategoryStatus(id,stat){
-        url = '<?php echo base_url();?>digitalauth/product/toggleproductStatus/'+id+'/' + stat;
+    function toggleCategoryStatus(id, stat) {
+        url = '<?php echo base_url();?>digitalauth/product/toggleproductStatus/' + id + '/' + stat;
         bootbox.confirm("<h4>Change Status</h4><hr><br><?php echo 'Confirm Status Change?'; ?>", function (result) {
             if (result) {
                 window.location.replace(url);
