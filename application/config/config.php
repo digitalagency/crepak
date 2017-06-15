d92ec76aa38bc260f0999457bae17c6de75dbd05
ayazdin <ayaz.din@gmail.com>
2017-06-15 12:30:00 +0545

    Config data changed

--- application/config/config.php
+++ application/config/config.php
@@ -23,12 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-if($_SERVER['HTTP_HOST']=="localhost" || $_SERVER['HTTP_HOST']=="127.0.1.1" || $_SERVER['HTTP_HOST']=="192.168.0.101")
-{
-    $config['base_url']='http://'.$_SERVER['HTTP_HOST'].'/cicms/';
-}
-elseif($_SERVER['HTTP_HOST']=="crepak.dev"){
-    $config['base_url']= 'http://crepak.dev';
+if($_SERVER['HTTP_HOST']=="pakcre.dev"){
+    $config['base_url']= 'http://pakcre.dev';
 }
 
 else{
@@ -380,7 +376,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
